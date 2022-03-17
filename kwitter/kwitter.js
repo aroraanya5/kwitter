0fb0b8c0f99a9e618a704b51091fd8b19809e404
Cabbage <96040913+aroraanya5@users.noreply.github.com>
2022-03-17 18:07:00 +1100

    Add files via upload

---
+++ kwitter/kwitter.js
@@ -0,0 +1,29 @@
+function addUser(){
+    user_name = document.getElementById("user_name").ariaValueMax;
+    localStorage.setItem("user_name", user_name);
+        window.location = "kwitter_room.html";
+}
+
+function addRoom()
+{
+    room_name = document.getElementById("room_name").value;
+
+    firebase.database().ref("/").child(rroom_name).update({
+        purpose : "adding room name"
+    });
+
+    localStorage.setItem("room_name", room_name);
+    window.location = "kwitter_page.html";
+}
+
+function send()
+{
+    msg = document.getElementById("msg").value;
+    firebase.database().ref(room_name).push({
+        name:user_name,
+        message:msg,
+        like:0
+    });
+
+    document.getElementById("msg").value  = "";
+}
